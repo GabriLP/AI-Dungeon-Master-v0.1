d51cfd4f61872e4db4bcbfb06b89e0ec7edc0de9
elson <elson.b@students.opit.com>
2025-03-16 18:25:28 +0100

    Organized the folders. Added JavaScript files

---
+++ JavaScript/config.js
@@ -0,0 +1,8 @@
+/* API configuration module*/
+
+export const AI_CONFIG = {
+    ENDPOINT: "https://api.mistral.ai/v1/chat/completions",
+    API_KEY: "sdk0pulQ5vmCEux1bvjSclcM013jbbOu",
+    MODEL: "mistral-small-latest"
+  };
+
