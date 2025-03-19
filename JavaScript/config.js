33dbc94f2bcde2ec60bf280ef87e0848ac8d6929
elson21 <elson.b@students.opit.com>
2025-03-19 22:23:57 +0100

    Deleted JavaScript/config.js

--- JavaScript/config.js
+++
@@ -1,8 +0,0 @@
-/* API configuration module*/
-
-export const AI_CONFIG = {
-    ENDPOINT: "https://api.mistral.ai/v1/chat/completions",
-    API_KEY: "sdk0pulQ5vmCEux1bvjSclcM013jbbOu",
-    MODEL: "mistral-small-latest"
-  };
-
